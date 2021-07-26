c5611e3290e9d7358c2316800c96698c983bb045
Jordan Nelson <nejrd@amazon.com>
2021-07-26 12:41:25 -0400

    update storage app

--- app/src/main/java/com/example/myamplifystorageapp/MyAmplifyStorageApp.kt
+++ app/src/main/java/com/example/myamplifystorageapp/MyAmplifyStorageApp.kt
@@ -2,12 +2,10 @@ package com.example.myamplifystorageapp
 
 import android.app.Application
 import android.util.Log
-import android.view.View
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin
-import java.io.File
 
 class MyAmplifyStorageApp : Application() {
 
@@ -25,16 +23,5 @@ class MyAmplifyStorageApp : Application() {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
         }
     }
-
-//    fun uploadFile(view: View) {
-//        val exampleFile = File(applicationContext.filesDir, "ExampleKey")
-//        exampleFile.writeText("Example file contents")
-//
-//        Amplify.Storage.uploadFile("ExampleKey", exampleFile,
-//            { Log.i("MyAmplifyApp", "Successfully uploaded: ${it.key}") },
-//            { Log.e("MyAmplifyApp", "Upload failed", it) }
-//        )
-//    }
-
 
 }
